8ba428eba5cb1e86b7a6e8034ced9b014977f12c
Christopher <christopherponce@hotmail.com>
2020-03-25 23:52:44 -0400

    added the following View Controllers: CarrierVC, Add PackageVC, PackageTableVC and StatusVC, added a networking directory for the usps client

---
+++ PackageTracker/Controller/AddPackageVC.swift
@@ -0,0 +1,30 @@
+//
+//  AddPackageVC.swift
+//  PackageTracker
+//
+//  Created by Christopher Ponce Mendez on 3/25/20.
+//  Copyright © 2020 Christopher Ponce Mendez. All rights reserved.
+//
+
+import UIKit
+
+class AddPackageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PackageTracker/Model/Networking/USPSClient.swift
@@ -0,0 +1,9 @@
+//
+//  USPSClient.swift
+//  PackageTracker
+//
+//  Created by Christopher Ponce Mendez on 3/25/20.
+//  Copyright © 2020 Christopher Ponce Mendez. All rights reserved.
+//
+
+import Foundation
