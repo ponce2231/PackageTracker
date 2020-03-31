b8e31da4a40fd093b017bd4d7a6298788d06bc61
Christopher Ponce Mendez <christopherponcedev@gmail.com>
2020-03-30 20:24:42 -0400

    created again the file Package

---
+++ PackageTracker/Model/Package.swift
@@ -0,0 +1,14 @@
+//
+//  Package.swift
+//  PackageTracker
+//
+//  Created by Christopher Ponce Mendez on 3/30/20.
+//  Copyright © 2020 Christopher Ponce Mendez. All rights reserved.
+//
+
+import Foundation
+
+struct Package {
+    let packageName: String
+    let packageNumber: String
+}
